9796ddbc78e857f02b52b8e8d4af9eab38cd96f6
DapperMickie <rustenhovenmick@gmail.com>
2024-04-22 15:02:36 +0200

    Added express js for heroku

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import { Octokit } from "@octokit/core";
 import fetch from 'node-fetch'
 import cron from 'node-cron'
 import dotenv from 'dotenv'
+import express from 'express'
+
+const PORT = process.env.PORT || 5001
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1> Hello, World! </h1>');
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
 
 dotenv.config();
 cron.schedule('* * * * *', () => {
@@ -18,6 +30,12 @@ cron.schedule('* * * * *', () => {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   }).then(good=>{
-    
-  }, rejected=>{})
+    console.log("updated code");
+    fetch("https://odablock-updater-2f1923601e95.herokuapp.com/")
+    .then(()=>{
+      console.log("pinged");
+    })
+  }, rejected=>{
+    console.log("Something went wrong?");
+  })
 });
